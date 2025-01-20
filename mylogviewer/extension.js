838ec1ec1125c7d08769b6538717dc16a651fadc
Himanshu Keshri <thehimanshukeshri@gmail.com>
2025-01-21 02:07:43 +0530

    Remove command

--- mylogviewer/extension.js
+++ mylogviewer/extension.js
@@ -105,60 +105,10 @@ function clearHighlights() {
 
 let decorationType;
 function activate(context) {
-  // Command to highlight text
-  let highlightDisposable = vscode.commands.registerCommand('mylogviewer.highlightText', async function () {
-    const textToHighlight = await vscode.window.showInputBox({
-      prompt: 'Enter the text to highlight',
-      placeHolder: 'Text to highlight...'
-    });
-
-    if (!textToHighlight) {
-      vscode.window.showErrorMessage('Please specify the text to highlight.');
-      return;
-    }
-
-    const editor = vscode.window.activeTextEditor;
-    if (!editor) {
-      return;
-    }
-
-    const text = editor.document.getText();
-    const regex = new RegExp(textToHighlight, 'gi');
-    decorationType = vscode.window.createTextEditorDecorationType({
-      backgroundColor: 'rgba(255,0,0,0.3)' // Red background color
-    });
-
-    const decorations = [];
-    let match;
-    while ((match = regex.exec(text))) {
-      const startPos = editor.document.positionAt(match.index);
-      const endPos = editor.document.lineAt(startPos.line).range.end;
-      const range = new vscode.Range(startPos, endPos);
-      const decoration = { range };
-      decorations.push(decoration);
-    }
-
-    editor.setDecorations(decorationType, decorations);
-  });
-
-  // Command to clear all highlights
-  let clearDisposable = vscode.commands.registerCommand('mylogviewer.clearHighlights', function () {
-    const editor = vscode.window.activeTextEditor;
-    if (!editor) {
-      return;
-    }
-
-    if (decorationType) {
-      editor.setDecorations(decorationType, []);
-    }
-  });
-
+ 
   // Register SidebarProvider
   const sidebarProvider = new SidebarProvider(context);
   context.subscriptions.push(vscode.window.registerWebviewViewProvider('mylogviewer-sidebar', sidebarProvider));
-
-  context.subscriptions.push(highlightDisposable);
-  context.subscriptions.push(clearDisposable);
 }
 
 function deactivate() { }
